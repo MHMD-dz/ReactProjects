1680fa9e575252786c285aaaf5164adf45700e2c
mohamed <mhmdhayza@gmail.com>
2025-06-25 11:32:20 +0100

    Project1 : just  a counter

--- component/Counter.jsx
+++ component/Counter.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react"
+import { useState , useEffect, useRef } from "react"
 
 export const Counter = () => {
     const [ count , setCount] = useState(0);
-    
+    const countRef = useRef(null);
+    useEffect(() => {
+    if (countRef.current) {
+        if(count < 0){
+            countRef.current.style.color = "red";
+        }else if(count > 0){
+            countRef.current.style.color = "green";
+        }else{
+            countRef.current.style.color = "white";
+        }}
+    }, [count]);
+
+    const Incr = () => {
+        setCount(count + 1);
+    }
+    const Decr = () => {
+        setCount(count - 1);
+    }
+    const Reset = () => {
+        setCount(0);
+    }
   return (
     <div>
-        <h1>Count : {count}</h1>
-        <button>Increase</button>
-        <button>Decrease</button>
-        <button>Reset</button>
+        <h1 className="count" ref={countRef} >Count : {count}</h1>
+        <button onClick={Incr} >Increase</button>
+        <button onClick={Decr} >Decrease</button>
+        <button onClick={Reset} >Reset</button>
     </div>
   )
 }
